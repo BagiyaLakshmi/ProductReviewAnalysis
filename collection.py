96796d968ef882c1d2f2b971ecb921cb041b890e
17percenter <mail2santhoshkannan@gmail.com>
2022-10-16 10:08:06 +0530

    updated scraper

--- collection.py
+++ collection.py
@@ -7,8 +7,6 @@ last modified:
 
 
 
-from re import S, X
-from telnetlib import XASCII
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
@@ -18,21 +16,26 @@ import time
 import os
 import csv
 
-def write_csv(scraped_list):
+def write_csv(csv_path,scraped_dict,header):
 
-    f = csv.writer(open('datasets/data.csv','a'))
+    f = csv.DictWriter(open(csv_path,'a'),fieldnames=header)
 
-    needs_header = os.stat('datasets/data.csv').st_size == 0
+    needs_header = os.stat(csv_path).st_size == 0
 
         # do stuff
 
         #if file needs a header, write headers
     if needs_header:
-        f.writerow(["title","store","rating","no_rating","reviews"])
+        f.writerow(header)
         needs_header = False
 
     # Then, write values
-    f.writerow(scraped_list)
+    if isinstance(scraped_dict,list):
+        for i in scraped_dict:
+            f.writerow(i)
+    else:
+        f.writerow(scraped_dict)
+        
 
 
 
@@ -46,51 +49,66 @@ def get_links():
     return links_list
 
 
-def get_reviews():
+def get_reviews(link_id):
     review_list = []
     try:
-        for i in range(10):
+        for i in range(50):
+            time.sleep(5)
             profile_name = driver.find_elements(By.XPATH,'//span[@class="a-profile-name"]')
             date = driver.find_elements(By.XPATH,'//span[@class="a-size-base a-color-secondary review-date"]')
             reviews= driver.find_elements(By.XPATH,'//span[@class="a-size-base review-text review-text-content"]/span[1]')
+            reviewers_rating = driver.find_elements(By.XPATH, '//i[@data-hook="review-star-rating"]/span[@class="a-icon-alt"]')
             print(len(reviews))
             for i in range(len(reviews)):
                 review_dict = {
+                    "id" : link_id,
                     "name" : profile_name[i+2].get_attribute("innerHTML"),
                     "data" : date[i+2].get_attribute("innerHTML"),
+                    "reviewers_rating":reviewers_rating[1].get_attribute("innerHTML").split()[0],
                     "review" : reviews[i].get_attribute("innerHTML"),
                 }
                 review_list.append(review_dict)
-            next_page = driver.find_element(By.XPATH,'//li[@class="a-last"]').click()
-    except:
+            next_page = driver.find_element(By.XPATH,'//li[@class="a-last"]')
+            next_page.click()
+    except Exception as e:
+        print(e)
         return review_list
     else:
         return review_list
 
-def scrap():
+def scrap(link_id):
     title = driver.find_element(By.XPATH,'//*[@id="productTitle"]').text
     store=driver.find_element(By.XPATH,'//div[@class="a-section a-spacing-none"]/a').text 
     store_new=" ".join((store.split(' '))[2:])
-
-    # rating = driver.find_element(By.XPATH,'/html/body/div[1]/div[2]/div[9]/div[4]/div[4]/div[3]/div/span[1]/span/span[1]/a/i[1]/span')
-    # rating = driver.find_element(By.XPATH,'(//i[@class="a-icon a-icon-star a-star-4-5"]/span[@class="a-icon-alt"])[1]').get_attribute("innerHTML").split()[0]
     rating = driver.find_element(By.XPATH,'(//span[@class="a-icon-alt"])[1]').get_attribute('innerHTML').split()[0]
     no_rating = driver.find_element(By.XPATH,'(//span[@id="acrCustomerReviewText"])[1]').text
+    info_dict = {
+        "id" : link_id,
+        "title" : title,
+        "store" : store_new,
+        "rating" : rating,
+        "no_rating" : no_rating,
+    }
+    write_csv('datasets/data.csv',info_dict,{"id":"id","title":"title","store":"store","rating":"rating","no_rating":"no_rating"})
+    
+    
     try:
         show_all =  driver.find_element(By.XPATH,'//*[@id="cr-pagination-footer-0"]/a')
         show_all.click()
     except:
         show_all = driver.find_element(By.XPATH,'//a[@class="a-link-emphasis a-text-bold"]')
         show_all.click()
-        
-    reviews = get_reviews()
-    write_csv([title, store_new, rating, no_rating, reviews])
+    
+    reviews = get_reviews(link_id)
+    write_csv("datasets/reviews.csv",reviews,{"id":"id","name":"name","data":"data","reviewers_rating":"reviewers_rating","review":"review"})
     
         
     
 if __name__ == "__main__":
     links_list = get_links()
-    for link in links_list[:10]:
-        print(link[0])
-        driver.get(link[0])
-        scrap()
+    link_id = 0
+    while(link_id<500):
+        print(links_list[link_id][0])
+        driver.get(links_list[link_id][0])
+        scrap(link_id)
+        link_id+=1
